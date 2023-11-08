2b8bfff6d5af85565b76300227e83227464c2b39
Gareth Bradley <gb@garethbradley.co.uk>
2023-11-08 16:30:02 +0000

    Completed joystick update

--- windows/flutter/generated_plugin_registrant.cc
+++ windows/flutter/generated_plugin_registrant.cc
@@ -6,11 +6,14 @@
 
 #include "generated_plugin_registrant.h"
 
+#include <dotup_flutter_active_window/dotup_flutter_active_window_plugin.h>
 #include <flutter_libserialport/flutter_libserialport_plugin.h>
 #include <screen_retriever/screen_retriever_plugin.h>
 #include <window_manager/window_manager_plugin.h>
 
 void RegisterPlugins(flutter::PluginRegistry* registry) {
+  DotupFlutterActiveWindowPluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("DotupFlutterActiveWindowPlugin"));
   FlutterLibserialportPluginRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("FlutterLibserialportPlugin"));
   ScreenRetrieverPluginRegisterWithRegistrar(
